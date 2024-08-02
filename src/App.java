1f1b738f00bf68516ed68a6a7044a14701d03f00
AntoneAl <aleman.antonio.95@gmail.com>
2024-08-01 22:21:34 -0600

    V3

--- src/App.java
+++ src/App.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hello, World!/n");
 
     //Prueba para crear arreglos
     String[] flores = {"Rosa", "Clavel", "Tulipan", "Girasol"};
     for(int i = 0; i < flores.length; i++){
         System.out.println(flores[i]);
     }
+    // Prueba de arreglo usuario ingresando valores sencillo
+    String[] canciones = new String[3];
+    System.out.println("Ingresa una canción: ");
+    canciones[0] = sc.nextLine();     
+    System.out.println("Ingresa una segunda canción: ");
+    canciones[1] = sc.nextLine();
+    System.out.println("Ingresa una tercera canción: ");
+    canciones[2] = sc.nextLine();
     }
 }
