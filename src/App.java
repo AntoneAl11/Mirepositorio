3a724909c1a0d3666ccb76e9c8f2aaacf2393230
AntoneAl <aleman.antonio.95@gmail.com>
2024-08-01 21:02:18 -0600

    V2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+    //Prueba para crear arreglos
+    String[] flores = {"Rosa", "Clavel", "Tulipan", "Girasol"};
+    for(int i = 0; i < flores.length; i++){
+        System.out.println(flores[i]);
+    }
     }
 }
